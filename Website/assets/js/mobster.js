a484599ab5731a87e66726312cbdf7971e3742fa
TechnoFuge <70207224+TechnoFuge@users.noreply.github.com>
2021-10-11 10:49:35 +0530

    Add files via upload

---
+++ Website/assets/js/mobster.js
@@ -0,0 +1,13 @@
+;$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 3,
+    center: true,
+    nav: true,
+    dots: false,
+    loop: true,
+  });
+});
+
+wow = new WOW();
+
+wow.init();
